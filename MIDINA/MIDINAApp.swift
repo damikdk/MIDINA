fc3098e0d64c763dcaa2a94e5c138f52497261e3
Damik Minnegalimov <damikdk@gmail.com>
2022-12-20 00:22:54 +0300

    Initial Commit

---
+++ MIDINA/MIDINAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDINAApp.swift
+//  MIDINA
+//
+//  Created by Damir Minnegalimov on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MIDINAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
